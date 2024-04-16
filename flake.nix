fd5665a38606df15efa9d9bf535856ed0a8484ca
set0xc3 <set0xc3@gmail.com>
2024-04-16 16:44:59 +0300

    backup

--- flake.nix
+++ flake.nix
@@ -3,56 +3,46 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
+  outputs = {nixpkgs, ...} @ inputs: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     devShells.${system}.default = pkgs.mkShell {
       LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
         pkgs.lib.makeLibraryPath [
-          pkgs.vulkan-extension-layer
-          pkgs.vulkan-validation-layers
-          pkgs.vulkan-utility-libraries
-          pkgs.vulkan-headers
-          pkgs.vulkan-loader
-          pkgs.vulkan-tools
-          pkgs.vulkan-tools-lunarg
-          pkgs.glslang
-          pkgs.shaderc
+          # pkgs.vulkan-extension-layer
+          # pkgs.vulkan-validation-layers
+          # pkgs.vulkan-utility-libraries
+          # pkgs.vulkan-headers
+          # pkgs.vulkan-loader
+          # pkgs.vulkan-tools
+          # pkgs.vulkan-tools-lunarg
 
-          pkgs.odin
-
-          pkgs.SDL2
-          pkgs.libGL
-
-          pkgs.xorg.libX11
+          # pkgs.odin
+          # pkgs.SDL2
+          # pkgs.libGL
+          # pkgs.xorg.libX11
         ]
       }";
 
       buildInputs = [
-        pkgs.renderdoc
-        pkgs.gpu-viewer
+        # pkgs.renderdoc
+        # pkgs.gpu-viewer
 
-        pkgs.vulkan-extension-layer
-        pkgs.vulkan-validation-layers
-        pkgs.vulkan-utility-libraries
-        pkgs.vulkan-headers
-        pkgs.vulkan-loader
-        pkgs.vulkan-tools
-        pkgs.vulkan-tools-lunarg
-        pkgs.glslang
-        pkgs.shaderc
+        # pkgs.vulkan-extension-layer
+        # pkgs.vulkan-validation-layers
+        # pkgs.vulkan-utility-libraries
+        # pkgs.vulkan-headers
+        # pkgs.vulkan-loader
+        # pkgs.vulkan-tools
+        # pkgs.vulkan-tools-lunarg
+        # pkgs.glslang # Khronos reference front-end for GLSL and ESSL
+        # pkgs.shaderc
 
         pkgs.odin
-
         pkgs.SDL2
-        pkgs.libGL
-
-        pkgs.xorg.libX11
+        # pkgs.libGL
+        # pkgs.xorg.libX11
       ];
 
       # shellHook = ''
